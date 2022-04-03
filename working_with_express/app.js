eff98493d5c4d06d6c46a0f2cc0582704c5d68e5
mtzsanad <mtz.sanad@gmail.com>
2022-04-03 22:55:02 +0200

    express body parser

--- working_with_express/app.js
+++ working_with_express/app.js
@@ -1,9 +1,13 @@
 //const http = require("http");
 const express = require("express");
+const bodyParser = require("body-parser");
 
 // First we create app and pass it to server
 //Init express as funciton invocation and we pass it to server
 const app = express();
+
+//Parser should be at begining of middlwares
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //Creates middleware, it takes a function that will be excuted for every request
 // next is a function that will be excuted to allow calling another function
@@ -14,6 +18,17 @@ app.use("/add", (req, res, next) => {
 
   //Calling next will call the next middleware in order from top to buttom
   next();
+});
+
+app.use("/form", (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="product" /><input type="submit" value="Send Request"></form>'
+  );
+});
+
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 // By default if path is not speified it routes to /
