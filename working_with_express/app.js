d0c3fa6fdc730806c8587fbc8b24e11b33635a94
mtzsanad <mtz.sanad@gmail.com>
2022-04-03 22:34:22 +0200

    express

---
+++ working_with_express/app.js
@@ -0,0 +1,31 @@
+//const http = require("http");
+const express = require("express");
+
+// First we create app and pass it to server
+//Init express as funciton invocation and we pass it to server
+const app = express();
+
+//Creates middleware, it takes a function that will be excuted for every request
+// next is a function that will be excuted to allow calling another function
+app.use((req, res, next) => {
+  //This will be logged and browser will spin coz no response
+  console.log("In middle ware");
+
+  //Calling next will call the next middleware in order from top to buttom
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("Another middle ware");
+
+  //This replaces res.write and res.header
+  res.send("<h1>Hello</h1>");
+});
+
+//app is valid request handler
+//const server = http.createServer(app);
+
+//server.listen(3001);
+
+// This replaces server and server listen
+app.listen(3001);
